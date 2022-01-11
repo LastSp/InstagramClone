4610014115ffe34a235a53e4c4b79d11ffd798ce
LastSp <lastspaise@gmail.com>
2022-01-11 15:32:48 +0300

    added Constants.swift

---
+++ InstagramClone/Utils/Contsants.swift
@@ -0,0 +1,10 @@
+//
+//  Contsants.swift
+//  InstagramClone
+//
+//  Created by Андрей Колесников on 11.01.2022.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("users")
